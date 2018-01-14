a68b62c6aeade96e25cae85010b51f8b951a0dba
Todd Olsen <todd.olsen@me.com>
2018-01-14 17:43:29 -0500

    Add files via upload

---
+++ init.js
@@ -0,0 +1,63 @@
+var turtle = null;
+var logo = null;
+var canvas;
+var form;
+var sprite;
+var textOutput;
+var fast;
+var out;
+var DelayTurtle;
+
+function setup() {
+    logo = new Logo();
+
+    fast = 5;
+    turtle = new DelayTurtle(canvas, sprite, fast, false);
+    logo.setTurtle(turtle);
+    logo.setTextOutput(textOutput);
+}
+
+function init(canvas_id, turtle_id, form_id, textoutput_id) {
+    canvas = document.getElementById(canvas_id);
+    form = document.getElementById(form_id);
+    textOutput = document.getElementById(textoutput_id);
+    sprite = document.getElementById(turtle_id);
+
+    canvas.style.width = 500;
+    canvas.width = 500;
+    canvas.style.height = 500;
+    canvas.height = 500;
+
+    setup();
+}
+
+function run(speed, drawbits) {
+    turtle.stop();
+    if (speed !== fast) {
+        fast = speed;
+        var newturtle = null;
+        // newturtle = new Turtle(canvas);
+
+        newturtle = new DelayTurtle(canvas, sprite, fast, drawbits);
+        logo.setTurtle(newturtle);
+        turtle = newturtle;
+    }
+
+    out = logo.run(form.code.value);
+
+    if (out && out.type === "error") {
+        alert(out.data);
+        setup();
+    }
+}
+
+function stop() {
+    turtle.stop();
+}
+
+function clearcanvas() {
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "rgb(255,255,255)";
+    ctx.fillRect(0, 0, 500, 500);
+    textOutput.innerHTML = "";
+}
